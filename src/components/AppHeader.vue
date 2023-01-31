94f3a87c6aaf5116cb4bc9be5e1c134e7ff0d3a0
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-01-31 13:00:35 +0100

    Add hover effect and pseudo element to nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,14 +12,17 @@ export default {
 
         <section id="main-nav">
             <nav class="d-flex justify-content-between align-items-center p-4 mx-4">
+
                 <!-- LOGO -->
                 <img src="../assets/img/logo-img-01.png" alt="Logo">
+
                 <!-- MAIN NAVBAR -->
+
                 <!-- possibile componente nav -->
                 <ul>
-                    <li v-for="link in mainNavLinks"><a href="#">{{ link }}</a></li>
+                    <li v-for="(link, i) in mainNavLinks"><a href="#">{{ link }}</a></li>
                     <li><a href="#"><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></a></li>
-                    <li><a href="#"></a><font-awesome-icon icon="fa-solid fa-bars" /></li>
+                    <li><a href="#"><font-awesome-icon icon="fa-solid fa-bars" /></a></li>
 
                 </ul>
             </nav>
@@ -30,14 +33,16 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
 header {
     height: 95vh;
     background-image: url(../assets/img/rev-slider-main-home-img-03.jpg);
     background-position: left;
-    background-size: cover;
 
     nav {
         height: 100px;
+
 
         img {
             width: 140px;
@@ -52,8 +57,25 @@ header {
             text-transform: uppercase;
 
             li {
-                margin: 0 10px;
-                font-weight: bolder;
+                margin: 0 20px;
+                font-weight: 500;
+
+                & a:hover {
+                    color: $orange;
+                    position: relative;
+
+                    &::before {
+                        position: absolute;
+                        right: 100%;
+                        bottom: 65%;
+                        transform: translateY(50%);
+                        content: "\2192 ";
+                        font-size: 40px;
+                        line-height: 0;
+                        padding: 5px;
+                    }
+
+                }
             }
         }
     }
